65d86c1c4eebcc942ea1ecb486c14119ef1adf1e
yonatan-h <yon019hl@gmail.com>
2023-02-19 12:09:06 +0300

    new mongo db ie changed url

--- main/src/back-end/common/mongo-url.js
+++ main/src/back-end/common/mongo-url.js
@@ -1,3 +1,3 @@
 const MONGO_URL =
-  "mongodb+srv://fonka:fonka@cluster0.onolpzi.mongodb.net/barbaric-regester?retryWrites=true&w=majority";
+  "mongodb+srv://sparkzone:sparkzone@cluster0.a86ulaj.mongodb.net/?retryWrites=true&w=majority";
 module.exports = { MONGO_URL };
