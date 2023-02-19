9285a646acb1158bea39613466d95ee7c6e31587
yonatan-h <yon019hl@gmail.com>
2023-02-19 11:38:23 +0300

    added age calculator to package.json

--- main/src/front-end/public/please-log-in.js
+++ main/src/front-end/public/please-log-in.js
@@ -1,5 +1,5 @@
 function pleaseLogin(fetchResult) {
-  if ((fetchResult.status = 401)) {
+  if (fetchResult.status === 401) {
     alert("please login ");
     throw new Error("not logged in");
   }
