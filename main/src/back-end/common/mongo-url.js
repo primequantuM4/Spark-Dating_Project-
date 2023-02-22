b11a6b5368c1d091e69b3fd61b5eac03711fca46
yonatan-h <yon019hl@gmail.com>
2023-02-22 16:46:48 +0300

    main deleted

--- main/src/back-end/common/mongo-url.js
+++
@@ -1,3 +0,0 @@
-const MONGO_URL =
-  "mongodb+srv://sparkzone:sparkzone@cluster0.a86ulaj.mongodb.net/?retryWrites=true&w=majority";
-module.exports = { MONGO_URL };
--- main/src/front-end/public/please-log-in.js
+++
@@ -1,6 +0,0 @@
-function pleaseLogin(fetchResult) {
-  if (fetchResult.status === 401) {
-    alert("please login ");
-    throw new Error("not logged in");
-  }
-}
